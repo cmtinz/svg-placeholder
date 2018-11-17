7eb83d199f22eb4099966bc3d8d64c2c7ee24de2
Carlos Martínez <cmtinz@gmail.com>
2018-11-17 17:46:30 -0300

    Added documentation and global svgPlaceholder function

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # svg-placeholder
+
+Function svgPlaceholder() returns the SVG code for a placeholder.
+
+## Examples
+
+### Basic Usage
+
+```js
+svgPlaceholder()
+/* 
+<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='250' height='250' style='background-color: white'>
+<rect width=125 height=125 x=62.5 y=62.5 fill=gray />
+</svg>
+*/
+```
+
+### Custom usage
+
+```js
+svgPlaceholder({shape: 'circle', fg: 'red'})
+/* 
+<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='250' height='250' style='background-color: white'>
+<circle cx=125 cy=125 fill=red r=62.5 />
+</svg>
+*/
+```
+
+## Supported attributes
+
+- shape: rectangle (default) | circle | text
+- embeded: if true escapes the XML declaration.
+- width: default 250
+- height: default 250
+- bg: default white
+- fg: default gray
+
--- svg-placeholder.js
+++ svg-placeholder.js
@@ -14,14 +14,12 @@ function header(att) {
 }
 
 function rectangle(att) {
-    var att = typeof att == 'undefined' ? new Object : att
     var svg = header(att)
     svg += `<rect width=${att.width/2} height=${att.height/2} x=${att.width/4} y=${att.height/4} fill=${att.fg} />\n` 
     return svg + "</svg>"
 }
 
 function circle(att) {
-    var att = typeof att == 'undefined' ? new Object : att
     var svg = header(att)
     var radius = [att.height, att.width].sort()[0] / 4
     svg += `<circle cx=${att.width/2} cy=${att.height/2} fill=${att.fg} r=${radius} />\n` 
@@ -29,9 +27,22 @@ function circle(att) {
 }
 
 function text(att) {
-    var att = typeof att == 'undefined' ? new Object : att
     att.text = att.text || `${att.width}x${att.height}`
     var svg = header(att)
     svg += `<text x='${att.width/2}' y='${att.width/2}' text-anchor='middle' dominant-baseline='middle' style='font-family: sans-serif' fill=${att.fg}>${att.text}</text>`
     return svg + "</svg>"
+}
+
+function svgPlaceholder (att) {
+    var att = typeof att == 'undefined' ? new Object : att
+    att.shape = att.shape || "rectangle"
+    switch(att.shape) {
+        case 'circle':
+            return circle(att)
+        case 'text':
+            return text(att)
+        default:
+            return rectangle(att)
+        
+    }
 }
